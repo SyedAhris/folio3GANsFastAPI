007a372318bb9c76a00ff70ff4de337d57e6b89a
SyedAhris <ahrissyed@gmail.com>
2022-07-28 23:51:30 +0500

    asdasda

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://urdugan-fd007.web.app/"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
