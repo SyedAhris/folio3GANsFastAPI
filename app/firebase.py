8c5b8e28d3e8d70f4fb481c586bd8fcfdd480471
Syed Muhammad Ahris <74341044+SyedAhris@users.noreply.github.com>
2023-02-08 10:45:39 +0500

    removed keys for public access

--- app/firebase.py
+++ app/firebase.py
@@ -2,14 +2,6 @@ import pyrebase
 import os
 from datetime import datetime
 firebaseConfig = {
-  'apiKey': "AIzaSyDexfRVzloKhyg4Jsxvlbnvg1GtjLRL488",
-  'authDomain': "urdugan-fd007.firebaseapp.com",
-  'projectId': "urdugan-fd007",
-  'databaseURL': "https://databaseName.firebaseio.com",
-  'storageBucket': "urdugan-fd007.appspot.com",
-  'messagingSenderId': "872703526029",
-  'appId': "1:872703526029:web:470644a5e72aa1c83ccec4",
-  'measurementId': "G-KBX7DHDMM8"
 }
 
 def initFirebase():
